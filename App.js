dfc298ad08d046012bb5ddb234dd2f441ad84327
Pankaj <pankajyadavpy069@gmail.com>
2025-01-18 23:33:02 +0530

    lecture 4

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
 
-const heading = React.createElement(
-    "h1", 
-    {
-        id: "title",
-    },
-    "Namaste Doosto..."
-);
+// const heading = React.createElement(
+//     "h1", 
+//     {
+//         id: "title",
+//     },
+//     "Namaste Doosto..."
+// );
 
-const heading2 = React.createElement(
-    "h2",
-    {
-        id: "title2",
-    },
-    "Namaskaar Everyone!"
-);
+// const heading2 = React.createElement(
+//     "h2",
+//     {
+//         id: "title2",
+//     },
+//     "Namaskaar Everyone!"
+// );
 
-const container = React.createElement(
-    "div",
-    {
-        id: "container",
-    },
-    [heading, heading2]
+// const container = React.createElement(
+//     "div",
+//     {
+//         id: "container",
+//     },
+//     [heading, heading2]
+// )
+
+const heading = () => {
+    return (
+        <h1 id="title">Namaste Doosto...</h1>
+    )
+}
+
+
+
+const container = (
+    <div id="container">
+        {heading()}
+        <h1 id="title">Namaste Doosto...</h1>
+        <h2 id="title2">Namaskaar Everyone!</h2>
+    </div>
 )
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
